bdaa1bab96d53a9b659d8399f5dfd7e92a2bc6d4
alwaysday1 <alwayday1@qq.com>
2023-02-27 12:12:28 +0800

    update web set

--- server/cmd/flashBox/main.go
+++ server/cmd/flashBox/main.go
@@ -5,9 +5,16 @@
 
 package main
 
-import "fmt"
+import (
+	"flash_box_server/internal/flashBox"
+	"os"
+)
 
 // Go 程序的默认入口函数(主函数).
 func main() {
-	fmt.Println("Hello F!!la!")
+	command := flashBox.NewFlashBoxCommand()
+
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
---
+++ server/internal/flashBox/flashBox.go
@@ -0,0 +1,103 @@
+// # Copyright 2023 LightTree <alwaysday1@qq.com>. All rights reserved.
+// # Use of this source code is governed by a MIT style
+// # license that can be found in the LICENSE file. The original repo for
+// # this file is https://github.com/alwaysday1/flashBox.
+
+package flashBox
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"flash_box_server/internal/pkg/log"
+	"flash_box_server/pkg/version/verflag"
+)
+
+var cfgFile string
+
+// NewFlashBoxCommand 创建一个 *cobra.Command 对象. 之后，可以使用 Command 对象的 Execute 方法来启动应用程序.
+func NewFlashBoxCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		// 指定命令的名字，该名字会出现在帮助信息中
+		Use: "flashBox",
+		// 命令的简短描述
+		Short: "A good Go practical project",
+		// 命令的详细描述
+		Long: `	A good Go practical project, used to create user with basic information.
+	Find more flashBox information at:
+	https://github.com/alwaysday1/flashBox#readme`,
+
+		// 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
+		SilenceUsage: true,
+		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// 如果 `--version=true`，则打印版本并退出
+			verflag.PrintAndExitIfRequested()
+
+			// 初始化日志
+			log.Init(logOptions())
+			defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
+			return run()
+		},
+		// 这里设置命令运行时，不需要指定命令行参数
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+
+			return nil
+		},
+	}
+	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
+	cobra.OnInitialize(initConfig)
+
+	// 在这里您将定义标志和配置设置。
+
+	// Cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the flashBox configuration file. Empty string for no configuration file.")
+
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// 添加 --version 标志
+	verflag.AddFlags(cmd.PersistentFlags())
+
+	return cmd
+}
+
+// run 函数是实际的业务代码入口函数.
+func run() error {
+	// 设置 Gin 模式
+	gin.SetMode(viper.GetString("runmode"))
+
+	// 创建 Gin 引擎
+	g := gin.New()
+
+	// 注册 404 Handler.
+	g.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"code": 10003, "message": "Page not found."})
+	})
+
+	// 注册 /healthz handler.
+	g.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+	// 创建 HTTP Server 实例
+	httpsrv := &http.Server{Addr: viper.GetString("addr"), Handler: g}
+
+	// 运行 HTTP 服务器
+	// 打印一条日志，用来提示 HTTP 服务已经起来，方便排障
+	log.Infow("Start to listening the incoming requests on http address", "addr", viper.GetString("addr"))
+	if err := httpsrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalw(err.Error())
+	}
+
+	return nil
+}
